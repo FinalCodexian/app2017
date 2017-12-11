afdddbb46ae9cae7470b3031cc44aa8fa45591b8
Luis Ventura <kbite1984@gmail.com>
2017-12-11 14:38:09 -0500

    aaa

--- application/controllers/ventas.php
+++ application/controllers/ventas.php
@@ -218,7 +218,7 @@ class Ventas extends CI_Controller {
           "excedido" => FALSE,
           "total" => $contador,
           "html" => (trim($this->input->post("excel")=="") ? $this->formatoReporte(["total"=>$contador,"data"=>$final]) : ''),
-          // "data" => $final,
+          "data" => $final,
           "totales" => $mTotales,
           "tot_notasNC" => $tot_notasNC,
           "tot_pendientes" => $tot_pendientes
